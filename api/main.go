515530d8d1a055f30daf4c7f1e61f23aecd6d2de
yaonur <ya_onur@hotmail.com>
2023-12-09 23:28:14 +0300

    ss

--- api/main.go
+++ api/main.go
@@ -1,15 +1,26 @@
 package main
 
 import (
+	"api/database"
+	"api/routes"
+
 	"github.com/gofiber/fiber/v2"
 )
 
+func welcome(c *fiber.Ctx) error {
+	return c.SendString("Api running fine")
+}
+
+func setupRoutes(app *fiber.App) {
+	app.Get("/api", welcome)
+	app.Post("/api/users", routes.CreateUser)
+	app.Get("/api/users", routes.GetUsers)
+}
+
 func main() {
+	database.ConnectDb()
 	app := fiber.New()
+	setupRoutes(app)
 
-	app.Get("/", func(c *fiber.Ctx) error {
-		return c.SendString("Hello, World!")
-	})
-
-	app.Listen(":3000")
+	app.Listen(":8000")
 }
